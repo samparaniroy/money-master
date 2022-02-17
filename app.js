537c6b5268533ba17a3417cab61f4391f00de643
Sampa Rani Roy <samparaniroy99@gmail.com>
2022-02-17 22:14:46 +0600

    money text

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-
- function calculateButtonPrice (){
+function calculateButtonPrice(){
     const foodPrice = document.getElementById('food-price-input');
     const rentPrice = document.getElementById('rent-price-input');
     const clothesPrice = document.getElementById('clothes-price-input');
     const foodExpense =  parseFloat(foodPrice.value);
     const rentExpense = parseFloat(rentPrice.value);
     const clothesExpense = parseFloat(clothesPrice.value);
-    const totalExpense = (foodExpense + rentExpense + clothesExpense);
-    const totalExpnseText = document.getElementById('total-expense');
-    totalExpnseText.innerText = totalExpense;
+    const foodExpensePrice = isNaN(foodExpense) ? 0 : foodExpense;
+    const rentExpensePrice = isNaN(rentExpense) ? 0 : rentExpense;
+    const clothesExpensePrice = isNaN(clothesExpense) ? 0 : clothesExpense;
+    const totalExpense = (foodExpensePrice + rentExpensePrice + clothesExpensePrice);
+    // total expense
+    const totalExpenseText = document.getElementById('total-expense');
+    totalExpenseText.innerText = totalExpense;
+    
+    // income input
     const incomeInput = document.getElementById('income-input');
-    const totalIncome = parseFloat(incomeInput.value);
-    const balance = totalIncome - totalExpense;
+    const totalIncomeInput = parseFloat(incomeInput.value);
+    const totalIncome = isNaN(totalIncomeInput) ? 0 : totalIncomeInput;
+    // balance total
+    balance = totalIncome - totalExpense;
+    if(totalIncome < totalExpense){
+        alert('Expense Amount is more than income amount')
+    }
     const totalBalance = document.getElementById('balance-total');
     totalBalance.innerText = balance;
-    foodPrice.value = '';
-    rentPrice.value = '';
-    clothesPrice.value = '';
+   
 }
+calculateButtonPrice();
+function saveAmount(){
+    const saveInput = document.getElementById('save');
+    const totalSaveInput = saveInput.value;
+    const totalSave = (balance*totalSaveInput) / 100;
+    const saveAmount = document.getElementById('save-amount');
+    saveAmount.innerText = totalSave;
+    if(balance < totalSave){
+        alert('You have not positive amount')
+    }
+    const totalBalance = document.getElementById('total-balance');
+    const totalBalanceAmount = balance-totalSave;
+    totalBalance.innerText = totalBalanceAmount;
+}
+
